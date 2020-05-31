dbfc6be059a6b6dbeec8c2479dc0225f931263f2
Ruth John <rumyra@gmail.com>
2020-05-31 11:15:12 +0100

    removetoggle

--- scripts/canvasvis.js
+++ scripts/canvasvis.js
@@ -41,8 +41,8 @@ ctx.lineJoin = 'round';
 const binSize = 128;
 const elAmount = Math.floor(binSize/3); // Returned frequncies is a third
 
-// create a new vis
-const vis = new Vis(binSize, '/beast.mp3');
+// soundcloudID: if you want to use a track from soundcloud pass in an id (as a string) here, otherwise mic is used:
+const vis = new Vis(binSize, '433074246');
 
 // setup our draw loop: THIS IS WHERE THE MAGIC HAPPENS!!
 vis.draw( () => {
@@ -50,14 +50,38 @@ vis.draw( () => {
 	ctx.clearRect(0, 0, screenDim.width, screenDim.height);
 	// loop over our frequencies and draw a shape for each one
 	vis.frequencies.forEach((f, i) => {
-		ctx.beginPath();
-		ctx.fillStyle = 'red';
-		ctx.arc(i*20, i*20, f, 0, 5);
-		ctx.fill();
-		ctx.closePath();
+		ctx.translate( (i%8)*220, Math.floor(i/6)*200);
+		let path = hexagon(f);
+		ctx.lineWidth = 10.0;
+		ctx.strokeStyle = `hsla(${i*8}, 50%, 50%, 1)`;
+		ctx.stroke(path);
+		ctx.resetTransform();
 	})
 	
 } )
+
+function degToRad(deg) {
+	return (2*Math.PI)/deg;
+}
+
+function hexagon(sideLength = 60) {
+	  // maths mother fucker
+	  const moveX = Math.sin(degToRad(30))*sideLength;
+	  const moveY = Math.cos(degToRad(30))*sideLength;
+
+	  const hexPath = new Path2D();
+
+	  // hexPath.moveTo(-sideLength/2, -moveY);
+	  hexPath.moveTo(0, 0);
+	  hexPath.lineTo(sideLength, 0);
+	  hexPath.lineTo(sideLength+moveX, moveY);
+	  hexPath.lineTo(sideLength, (moveY*2));
+	  hexPath.lineTo(0, (moveY*2));
+	  hexPath.lineTo(-moveX, moveY);
+	  hexPath.lineTo(0, 0);
+
+	  return hexPath;
+}
 
 
 // ===================== CONTROLS edit here if you want to start/stop multiple vis
